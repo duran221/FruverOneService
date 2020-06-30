5d3062acf614f9e6a5436f176f29305e87a3511c
LeonardGonzG <gonzalezgutierrezleonardo1@gmail.com>
2020-06-30 18:44:27 -0500

    Estructura proyecto MVC - APIRest

---
+++ FruverOneWebService/Datos/ControlRepository/IProcessInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Datos.ControlRepository
+{
+    interface IProcessInformation
+    {
+
+        JsonDocument queryInformation();
+
+
+
+
+    }
+}
