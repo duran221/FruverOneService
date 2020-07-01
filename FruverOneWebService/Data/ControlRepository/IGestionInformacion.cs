0dd45fb1e2b28a882f2a96655ac4e042257750d5
LeonardGonzG <gonzalezgutierrezleonardo1@gmail.com>
2020-06-30 19:55:04 -0500

    Iniciando capa de datos

---
+++ FruverOneWebService/Data/ControlRepository/IGestionInformacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Data.ControlRepository
+{
+    interface IGestionInformacion
+    {
+
+        bool actualizar(String query);
+        bool eliminar(String query);
+        bool insertar(String query);
+
+        //Json consulta(String query);
+
+    }
+}
