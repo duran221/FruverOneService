361b25e80ccef449d80a1b066cdc6e37507326f9
yimmydiaz <yimmy.1701523504@ucaldas.edu.co>
2020-07-29 19:35:40 -0500

    cambios fusionados

---
+++ FruverOneWebService/APIFruverOne/Controllers/RegisterCustomerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Business.Control;
+using Domain.Class;
+using Newtonsoft.Json.Linq;
+
+namespace APIFruverOne.Controllers
+{
+    public class RegisterCustomerController : ApiController
+    {
+        //Variables de instancia de las clases de control de la capa de Negocio:
+        private readonly ControlRegisterCustomer controlRegister;
+        private readonly ControlGetCustomer controlGetCustomer;
+
+        public RegisterCustomerController()
+        {
+            controlRegister = new ControlRegisterCustomer();
+            controlGetCustomer = new ControlGetCustomer();
+        }
+
+
+
+        /// <summary>
+        /// Permite obtener información de todos los clientes (Customer) registrados en el sistema
+        /// Ruta para acceso: GET: api/RegisterCustomer
+        /// </summary>
+        /// <returns></returns>
+        public bool Get()
+        {
+            var costumers = controlGetCustomer.GetCustomers();
+            return true;
+        }
+
+
+        // 
+        /// <summary>
+        /// Permite obtener la información de un cliente(Customer) especifíco usando como criterio su identificación
+        /// Ruta para acceso: GET: api/RegisterCustomer/5
+        /// </summary>
+        /// <param name="documentCustomer">Documento de identificación del cliente (Customer)</param>
+        /// <returns>JSON con la información del cliente solicitado</returns>
+        public IHttpActionResult Get(string documentCustomer)
+        {
+            Customer customer = controlGetCustomer.GetCustomer(documentCustomer);
+            return Ok(customer);
+        }
+
+
+        /// <summary>
+        /// Responde a una solicitud de registro de un cliente (Customer), gestiona el registro del usuario en la base de datos
+        /// Ruta para accesar: api/RegisterCustomer
+        /// </summary>
+        /// <param name="dataCustomer">JSON con los datos del cliente (Customer) a registrar</param>
+        /// <returns>Verdadero si el registro fué exitoso, Falso de lo contrario</returns>
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Post(JObject dataCustomer)
+        {
+            var condition = controlRegister.RegisterCostumer(dataCustomer);
+
+            return condition ? Ok("True"): Ok("False");
+        }
+      
+    }
+}
---
+++ FruverOneWebService/FruverOneWebClient/RequestAPI/Request.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace FruverOneWebClient.RequestAPI
+{
+    public static class Request
+    {
+
+        /// <summary>
+        /// Genera y envia una solicitud http a el servicio web de la plataforma APIFruverOne
+        /// </summary>
+        /// <typeparam name="T">View Model que se va a enviar al web Service</typeparam>
+        /// <param name="url">Dirección del servicio web al cual se realiza la petición</param>
+        /// <param name="objectRequest">Objeto que se va a enviar a el web service para su posterior procesamiento</param>
+        /// <param name="method">Indica el tipo de petición (Get o Post), por defecto se establece POST</param>
+        /// <returns></returns>
+        public static Response Send<T>(string url, T objectRequest, string method = "POST")
+        {
+            Response responseApi = new Response();
+            try
+            {
+                
+                //serializamos el objeto en una cadena JSON: 
+                string json = JsonConvert.SerializeObject(objectRequest);
+
+                WebRequest request = CrearRequest(url, method, json);
+                //Se envia la petición http al web service y se obtiene una referencia de la respuesta:
+                var httpResponse = (HttpWebResponse)request.GetResponse();
+
+                string result = LeerArchivo(httpResponse);
+                responseApi.Success = 200;
+                //y aquí va nuestra respuesta, la cual es lo que nos regrese el sitio solicitado
+                responseApi.Data = result;
+            }
+            catch (Exception e)
+            {
+                responseApi.Success = 0;
+                //en caso de error lo manejamos en el mensaje
+                responseApi.Message = e.Message;
+            }
+
+            return responseApi;
+        }
+
+        /// <summary>
+        /// Lee y almacena en una cadena la respuesta devuelta por el servicio web
+        /// </summary>
+        /// <param name="httpResponse">Objeto http recibido como respuesta del servicio web</param>
+        /// <returns>Cadena de texto con la respuesta del servicio</returns>
+        private static string LeerArchivo(HttpWebResponse httpResponse)
+        {
+            string result;
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Crea una solicitud HttpRequest con los atributos necesarios para ser enviada al WebService
+        /// </summary>
+        /// <param name="url">dirección del servicio web al cual se realiza la petición</param>
+        /// <param name="method">el tipo de petición que se realiza, puede ser GET,POST,PUT,DELETE</param>
+        /// <param name="json">Cadena con la Información que se desea enviar al WebService</param>
+        /// <returns></returns>
+        private static WebRequest CrearRequest(string url, string method, string json)
+        {
+            //Instanciación del objeto que me permite realizar la petición a el web service (http).
+            WebRequest request = WebRequest.Create(url);
+            //headers
+            request.Method = method;
+            request.PreAuthenticate = true;
+            request.ContentType = "application/json;charset=utf-8'";
+            request.Timeout = 10000; //esto es opcional
+                                     //Se escribe el objeto JSON en el objeto request:
+            request= EscribirArchivo(json, request);
+
+            return request;
+        }
+
+        
+        /// <summary>
+        /// Escribe la cadena JSON dentro de la solicitud Http que será enviada al WebService
+        /// </summary>
+        /// <param name="json">Cadena con los datos del objeto a incluir en la solicitud</param>
+        /// <param name="request">Solicitud Http que será enviada al WebService</param>
+        /// <returns>Solicitud Http con la información del Json contenido</returns>
+        private static WebRequest  EscribirArchivo(string json, WebRequest request)
+        {
+            if (json != "null")
+            {
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamWriter.Write(json);
+                    //Cerrando la escritura del archivo:
+                    streamWriter.Flush();
+                }
+            }
+
+            return request;
+        }
+    }
+}
