89896be1b28f4447c5252b58dccc62b7f452a846
duran221 <cristianfernandoduran@gmail.com>
2020-08-25 21:29:12 -0500

    Funcionalidad Buscar Producto y Autenticar Usuario integrada

--- FruverOneWebService/FruverOneWebClient/App_Start/RouteConfig.cs
+++ FruverOneWebService/FruverOneWebClient/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FruverOneWebClient
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "Frutas", id = UrlParameter.Optional }
             );
         }
     }
